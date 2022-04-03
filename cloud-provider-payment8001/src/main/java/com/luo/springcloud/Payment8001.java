01351e7eb6110c3fb48462d61d1886fda5ef71cc
luo <2770997350@qq.com>
2022-04-03 20:37:40 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/luo/springcloud/Payment8001.java
@@ -0,0 +1,19 @@
+package com.luo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Payment8001
+ * @Description TODO
+ * @Author l'l
+ * @Date 2022/4/3 20:28
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Payment8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class,args);
+    }
+
+}
